b4468a456c5f4428487a40ab88e4b162fe379a79
ADMIN-PC\Admin <thienphuc1052004@gmail.com>
2026-03-23 15:08:35 +0700

    ad led

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include <Arduino.h>
-
-int led_counter = 0;
-int led_status = HIGH;
+#include <math.h>
 
 void setup() {
     pinMode(2, OUTPUT);
 }
 
 void loop() {
-    led_counter++;
-    if (led_counter == 100) {
-        led_counter = 0;
-        led_status = !led_status;
-        digitalWrite(2, led_status);
-    }
-    delay(10);
+    // Tính toán delay theo hàm sin để tạo hiệu ứng nháy nhanh/chậm
+    float time = millis() / 1000.0;
+    int blink_delay = 275 + (int)(225 * sin(time));
+
+    digitalWrite(2, LOW);  // Bật LED
+    delay(blink_delay);
+    digitalWrite(2, HIGH); // Tắt LED
+    delay(blink_delay);
 }
 
 // // put function declarations here:
